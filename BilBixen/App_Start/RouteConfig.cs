d681b10f03971c56522577e8be245fa329447396
Minik Gaarde Lambrecht <mini0040@edu.sde.dk>
2019-04-25 15:23:08 +0200

    Update: - Routes and pages have been made. - Navbar created.

--- BilBixen/App_Start/RouteConfig.cs
+++ BilBixen/App_Start/RouteConfig.cs
@@ -16,7 +16,15 @@ namespace BilBixen
             };
             routes.EnableFriendlyUrls(settings);
 
-            routes.MapPageRoute("Home", "", "~/Pages/Default.aspx");
+            routes.MapPageRoute("Home_Page", "", "~/Pages/Default.aspx");
+            routes.MapPageRoute("About_Page", "About", "~/Pages/About.aspx");
+            routes.MapPageRoute("Contact_Page", "Contact", "~/Pages/Contact.aspx");
+
+            routes.MapPageRoute("Login_Page", "Login", "~/Pages/Login.aspx");
+            routes.MapPageRoute("ForgotPassword_Page", "ForgotPassword", "~/Pages/ForgottenPassword.aspx");
+
+            routes.MapPageRoute("Sale_Page", "Sale", "~/Pages/CarsForSale.aspx");
+            routes.MapPageRoute("Sell_Page", "Sell", "~/Pages/SellYourCar.aspx");
         }
     }
 }
---
+++ BilBixen/Pages/About.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="BilBixen.Pages.About" %>
+
+<asp:Content ID="Body_Content" ContentPlaceHolderID="MainContent" runat="server">
+
+</asp:Content>
